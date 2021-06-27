584db5942c60977e2c95a36569bc76ea5bfaf79a
skar1707 <60777006+skar1707@users.noreply.github.com>
2021-06-27 23:53:09 +0530

    Finding Median of Vector

---
+++ Array/Median.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+double mean(vector<int> vec){
+
+   double med = 0;
+   int size = vec.size();
+   sort(vec.begin(),vec.end());
+   if (size % 2 != 0)
+      med = (double)vec[size/2];
+   else
+      med = (double)(arr[(size-1)/2] + arr[size/2])/2.0;
+
+   return med;
+}
+
+int main(){
+   vector<int> v = {3,5,2,1,7,8};
+
+   cout << "Median is : " << median(v)<< endl;
+   return 0;
+}
